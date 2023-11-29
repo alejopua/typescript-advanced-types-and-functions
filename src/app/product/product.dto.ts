2f6804ae86cf2ce9f45d9f4ac30071477ac121db
alejopua <alejopua@gmail.com>
2023-11-28 19:28:10 -0500

    [add](class): comment description

--- src/app/product/product.dto.ts
+++ src/app/product/product.dto.ts
@@ -1,5 +1,6 @@
 import { Product } from "./product.model";
 
+// utility type Omit and Pick - Dto's para creación
 export interface CreateProductDto extends Omit<Product, 'id' | 'createdAt' | 'updatedAt' | 'category'> {
     categoryId: string;
 }
